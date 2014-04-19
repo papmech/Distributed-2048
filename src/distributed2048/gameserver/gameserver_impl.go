c5525be4061e41337f640728d7e5b7a1130a6c5c
Lingzhang <jlz777@gmail.com>
2014-04-19 02:22:26 -0400

    small

--- src/distributed2048/gameserver/gameserver_impl.go
+++ src/distributed2048/gameserver/gameserver_impl.go
@@ -2,4 +2,13 @@ package gameserver
 
 import (
 	"fmt"
+	"os"
 )
+
+const (
+	ERROR_LOG bool = true
+	DEBUG_LOG bool = true
+)
+
+var LOGV = util.NewLogger(DEBUG_LOG, "DEBUG", os.Stdout)
+var LOGE = util.NewLogger(ERROR_LOG, "ERROR", os.Stderr)

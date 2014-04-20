e114eb077b5fc35c95698218510dec5883c1d53c
Lingzhang <jlz777@gmail.com>
2014-04-20 14:15:04 -0400

    removed huge folder

--- src/distributed2048/client/js/connection_manager.js
+++ src/distributed2048/client/js/connection_manager.js
@@ -1,8 +1,8 @@
 function ConnectionManager() {
     console.log("hello from connectionManager");
     $.get("http://localhost:15340", function(data, status) {
-        alert("managed to get a response from the server")
-        alert(data)
+        console.log("managed to get a response from the server")
+        console.log(data)
     });
     console.log("Completed Get Request")
 }
